af27718c6fe12bd42d0d11bbb3fefd43373ae26c
Carson <carsonbarnes98@gmail.com>
2023-11-02 11:41:37 -0400

    added links and nested

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,26 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <HomeLayout />,
+    children:[
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'cocktail',
+        element: <Cocktail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
   },
-  {
-    path: '/about',
-    element: <About />,
-  },
+  
 ]);
 
 const App = () => {
