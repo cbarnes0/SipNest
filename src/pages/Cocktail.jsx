ffec3de9ab54358644f46345063c318b19f9f288
Carson <carsonbarnes98@gmail.com>
2023-11-06 10:16:25 -0500

    single cocktail page rendering

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,8 +1,8 @@
-import { useLoaderData, useRouteLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 import axios from "axios";
 import Wrapper from '../assets/wrappers/CocktailPage';
 const singleCocktailUrl = 
-    'https://www.thecocktaildb.com/apis/json/v1/1/lookup.php?i=';
+    'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
   
 export const loader = async({params}) => {
     const { id } = params
@@ -12,6 +12,50 @@ export const loader = async({params}) => {
 
 const Cocktail = () => {
     const { id, data } = useLoaderData()
-    return <h1>Cocktail</h1>
+
+    const singleDrink = data.drinks[0];
+
+    const {
+        strDrink:name, 
+        strDrinkThumb:image, 
+        strAlcoholic:info,
+        strCategory: category,
+        strGlass: glass,
+        strInstructions: instructions,
+    } = singleDrink
+
+    return <Wrapper>
+        <header>
+            <Link to='/' className="btn">
+                back home
+            </Link>
+            <h3>{name}</h3>
+        </header>
+        <div className="drink">
+            <img src={image} alt={name} className="img" />
+            <div className="drink-info">
+                <p>
+                    <span className="drink-data">name :</span>
+                    {name}
+                </p>
+                <p>
+                    <span className="drink-data">category :</span>
+                    {category}
+                </p>
+                <p>
+                    <span className="drink-data">info :</span>
+                    {info}
+                </p>
+                <p>
+                    <span className="drink-data">glass :</span>
+                    {glass}
+                </p>
+                <p>
+                    <span className="drink-data">instructions :</span>
+                    {instructions}
+                </p>
+            </div>
+        </div>
+    </Wrapper>
 };
 export default Cocktail;
