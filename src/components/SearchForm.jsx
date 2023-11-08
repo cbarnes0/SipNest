3808e273c9d6331be536a977706ec82389785763
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-08 10:52:51 -0500

    search form functionality maintained

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,17 +1,17 @@
 import Wrapper from '../assets/wrappers/SearchForm';
 import { Form, useNavigation } from 'react-router-dom';
 
-const SearchForm = () => {
+const SearchForm = ({searchTerm}) => {
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
     return (
         <Wrapper>
-            <Form>
+            <Form className='form'>
                 <input 
                     type='search' 
                     name='search' 
                     className='form-input' 
-                    defaultValue='vodka'
+                    defaultValue={searchTerm}
                 />
                 <button type='submit' className='btn' disabled={isSubmitting}>
                     {isSubmitting ? 'searching...' : 'search'}
