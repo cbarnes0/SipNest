943402fe515e3ee4a041171aada424361b1e7ed5
Carson Barnes <117684288+cbarnes0@users.noreply.github.com>
2023-11-08 10:35:42 -0500

    search form added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import CocktailList from "../components/CocktailList";
+import SearchForm from '../components/SearchForm';
 
 const cocktailSearchUrl =
 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
@@ -13,8 +14,11 @@ export const loader = async () => {
 
 const Landing = () => {
     const { drinks, searchTerm } = useLoaderData();
-    return <>
+    return (
+    <>
+        <SearchForm />
         <CocktailList drinks={drinks}/>
     </>
+    );
 };
 export default Landing; 
